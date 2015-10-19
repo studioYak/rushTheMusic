adc35a475c31b784849ebcc4594eb4035aeba7fa
Hugo <hugo.leray.scheidt@gmail.com>
2015-10-19 15:04:31 +0200

    Revert "Ajout d'arme au lancier"
    This reverts commit 50163bfa6c2e5901e6e9d2e0e177d6809bf2f3b2.

--- Assets/game/units/scripts/NPC/Lancer/BasicLancer.cs
+++ Assets/game/units/scripts/NPC/Lancer/BasicLancer.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class BasicLancer : Lancer {
 	
-	public GameObject lancePrefab;
-	
 	// Use this for initialization
 	void Start () {
 		gameObject.GetComponent<Renderer>().material.color = Color.black;
@@ -17,6 +15,6 @@ public class BasicLancer : Lancer {
 
 	public BasicLancer()
 		:base(2.0f, 5, 30, 300, 10, "cac", "anonymous"){
-		base.weapon = Object.Instantiate(lancePrefab);
+
 	}
 }
